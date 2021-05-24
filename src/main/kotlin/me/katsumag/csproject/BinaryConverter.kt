e83acf27e7b7c8ad4e4efc30bfc06bd9a4daeb5f
James <goude47@gmail.com>
2021-05-24 19:52:18 +0100

    Finish

--- src/main/kotlin/me/katsumag/csproject/BinaryConverter.kt
+++ src/main/kotlin/me/katsumag/csproject/BinaryConverter.kt
@@ -2,28 +2,42 @@ package me.katsumag.csproject
 
 class BinaryConverter(private val binary: String) {
 
-    fun convert(exponent: Boolean): Int {
+    private fun calculateBases() = binary.indices.map { 1 shl it }.toMutableList().asReversed()
+    private fun calculateBases(newBinary: String) = newBinary.indices.map { 1 shl it }.toMutableList().asReversed()
 
-        val reference = binary.indices.map { 1 shl it }.toMutableList()
+    private fun calculateReciprocalBases(): List<Double> {
+        val bases = calculateBases(binary + "0").asReversed()
+        bases.removeAt(0)
+        return bases.map { 1 / it.toDouble() }
+    }
 
-        val convertedBinary = binary.reversed().map { Character.getNumericValue(it) }
+    private fun flipFirst(list: MutableList<Int>) = list.updated(0, list[0] * -1)
 
-        if (exponent) reference[reference.lastIndex] *= -1
-
-        val result = reference zip convertedBinary
-        val table = result.toMap()
-
-        println(reference)
-        println(table)
-
-        var total = 0
+    private fun convertToDenary(bases: List<Number>): Double {
+        var total = 0.0
+        val table = (bases zip binary.map { Character.getNumericValue(it) }).toMap()
 
         table.entries.forEach {
             if (it.value == 1) {
-                total += it.key
+                total += it.key.toDouble()
             }
         }
 
         return total
+
     }
+
+    fun toDenary() = convertToDenary(calculateBases())
+    fun toDenaryFirstNegative() = convertToDenary(flipFirst(calculateBases()))
+    fun toDenaryReciprocals() = convertToDenary(calculateReciprocalBases())
+
+}
+
+fun <E> Iterable<E>.updated(index: Int, elem: E) = mapIndexed { i, existing ->  if (i == index) elem else existing }
+
+fun insertString(insert: String, index: Int, string: String) = string.substring(0, index) + insert + string.substring(index, string.lastIndex+1)
+
+fun shiftPoint(mantissa: String, exponent: Int): String {
+    val parts = mantissa.split(".")
+    return parts[0] + insertString(".", exponent, parts[1])
 }
--- src/main/kotlin/me/katsumag/csproject/routes/ComponentRoutes.kt
+++ src/main/kotlin/me/katsumag/csproject/routes/ComponentRoutes.kt
@@ -1,20 +1,26 @@
 package me.katsumag.csproject.routes
 
-import me.katsumag.csproject.models.*
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import me.katsumag.csproject.BinaryConverter
+import me.katsumag.csproject.models.Component
+import me.katsumag.csproject.shiftPoint
 
 fun Route.componentRouting() {
     route("/api") {
         post {
             val component = call.receive<Component>()
-            val mantissa = BinaryConverter(component.mantissa).convert(false)
-            val exponent = BinaryConverter(component.exponent).convert(true)
-            call.respondText("Mantissa = $mantissa. Exponent = $exponent", status = HttpStatusCode.OK)
+            val exponent = BinaryConverter(component.exponent).toDenaryFirstNegative()
+            val mantissa = shiftPoint(component.mantissa, exponent.toInt())
+            val parts = mantissa.split(".")
+            val first = BinaryConverter(parts[0]).toDenaryFirstNegative()
+            val second = BinaryConverter(parts[1]).toDenaryReciprocals()
+            val total = first + second
+
+            call.respondText("Mantissa = $mantissa\nExponent = $exponent\nTotal = $total", status = HttpStatusCode.OK)
         }
     }
 }
